af088c3980d16c4e4112467bf4ae68fdf3490509
Anan Sarah <anansarahnikita27@gmail.com>
2019-04-03 14:11:18 +0600

    add: puppeteer,cheerio

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,15 @@
 const functions = require('firebase-functions')
 const cors = require('cors')
 const express = require('express')
-// const scraper = require('./scraper')
+const scraper = require('./scraper')
 
 const app = express()
 
 app.use(cors({origin: true}))
 
 app.get('/scrape', (req, res) => {
-
-  res.send('Web Scraping and Storing Data: Puppeteer, Firebase and MongoDB: Part 1')
+  scraper(res)
 })
-
 
 exports.api = functions
   .runWith({memory: '1GB'})
---
+++ functions/scraper.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+const $ = require('cheerio')
+
+const url = 'https://www.reddit.com/'
+
+const DailyNews = function(res) {
+  var reddit = {
+    headlines: [],
+  }
+
+  return puppeteer
+    .launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+    .then(function(browser) {
+      browser
+        .newPage()
+        .then(function(page) {
+          page
+            .goto(url, {
+              waitUntil: 'load',
+              timeout: 300000,
+            })
+            .then(function() {
+              page
+                .content()
+                .then(function(content) {
+                  $('h2', content).each(function() {
+                    let headline = $(this).text()
+                    let obj = {text: headline}
+
+                    console.log(headline)
+
+                    reddit.headlines.push(obj)
+                  })
+                  browser.close()
+                  res.json(reddit)
+                })
+                .catch(function(error) {
+                  console.log('reddit content error')
+                  console.log(error)
+                })
+            })
+            .catch(function(error) {
+              console.log('reddit goto error')
+              console.log(error)
+            })
+        })
+        .catch(function(error) {
+          console.log('reddit newpage error')
+          console.log(error)
+        })
+    })
+    .catch(function(error) {
+      console.log('inside reddit')
+      console.log(error)
+    })
+}
+
+module.exports = DailyNews
