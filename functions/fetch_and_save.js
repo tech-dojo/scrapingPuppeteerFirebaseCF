02178203e27dd9761de9eec6663c666167e1bc0f
Rabbi Iqbal <rabbi425@gmail.com>
2019-04-07 11:18:50 +0600

    fix: request npm issue

--- functions/fetch_and_save.js
+++ functions/fetch_and_save.js
@@ -1,5 +1,5 @@
-const mongo = require('mongodb');
-const request = require('request');
+const mongo = require('mongodb')
+const request = require('request')
 
 let url = 'https://us-central1-scrapingdemo-92341.cloudfunctions.net/api/scrape'
 let db_url = "mongodb://localhost:27017"
@@ -13,27 +13,39 @@ mongo.MongoClient.connect(db_url,
             console.log('Connected to MongoDB')
             db = client.db('scrapedData')
             db.createCollection('test', (err, collection) => {
+                console.log("Creating collection...")
                 if (err) {
                     console.log("Error: ", err)
                 }
-            });
-
+                else {
+                    fetchData()
+                }
+            })
         }
     })
 
 
-const save = (form_data) => {
-
+const save = (data) => {
+    console.log("Saving to mongoDB...")
     let collection = db.collection('test')
-    collection.insertOne(form_data)
+    collection.insertOne(JSON.parse(data)).then(() => {
+        console.log("Data has been saved to mongoDB")
+    })
 
 }
 
+const fetchData = () => {
+    console.log("Fetching data...")
+    request(url, (err, res, body) => {
 
-request(url, { json: true }, (err, res, body) => {
+        if (res.statusCode == 200) {
+            save(body)
+        }
 
-    if (err) { return console.log(err); }
-    console.log(body)
-    save(body)
+        else {
+            console.log("Something went wrong!")
+        }
 
-})
+    })
+
+}
